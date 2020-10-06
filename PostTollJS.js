4300f4da30b34c2986537bac30dd1045eb7c6585
Dablioweb <61304453+dablioweb@users.noreply.github.com>
2020-10-06 15:38:31 -0300

    Update PostTollJS.js

--- PostTollJS.js
+++ PostTollJS.js
@@ -2,51 +2,28 @@
  * Arquivo que contém as funções js do PostTool a serem realizadas no navegador.
  */
 
+var qtdGroupsDivisions; // armazena quantidade de divisões (groups you managed, other ...) de grupos que existem
+var amountGroups; // armazena a quantidade de grupos que o usuário faz parte renderizados na tela
+
 /**
- * Função delay
+ * Pega a quantidade de grupos que o usuário faz parte renderizamos na tela.
  */
-function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-/**
- * Exibe todos os grupos que o usuário faz parte.
-*/
-var qtd; // armazena a quantidade de divisões de grupos que existem
-var amountGroups; // armazena a quantidade de grupos que o usuário faz parte
-
-async function showGroups(){
+function getDisplayedGroups(){
 	// pega a quantidade de divisões (groups you managed, other ...) de grupos que existem
-	qtd = document.getElementsByClassName("_2pip").length;
+	qtdGroupsDivisions = document.getElementsByClassName("_2pip").length;
 
 	// pega a quantidade de grupos já renderizados na tela
-	amountGroups = document.getElementsByClassName("_2pip")[qtd - 1].children.length;
+	amountGroups = document.getElementsByClassName("_2pip")[qtdGroupsDivisions - 1].children.length;
 
-	// faz o processo de carregar até que todos os grupos estejam renderizados na tela
-	do {
-		// rola para p final da página
-		window.scroll(0, document.body.scrollHeight)
+	// rola para p final da página
+	window.scroll(0, document.body.scrollHeight);
 
-		// espera 5 segundos
-		await delay(5000);
-
-		// variavel auxiliar para pegar a nova quantidade de grupos renderizados na tela após a rolagem da página
-		var amountGroupsAux = document.getElementsByClassName("_2pip")[qtd - 1].children.length;
-
-		// verifica se a nova quantidade de grupos é diferente a quantidade que já foi capturada.
-		// Se for, diferente, atualiza a quantidade de grupos capturados, reseta a variavel auxiliar para que continue o processo e na próxima
-		// vez compare com a quantidade anterior já capturada. Se for igual, sai do laço porque já renderizou na tela todos os grupos que o usuário
-		// faz parte
-		if (amountGroups != amountGroupsAux){
-				amountGroups = amountGroupsAux;
-				amountGroupsAux = 0;
-		}
-
-	}while(amountGroups != amountGroupsAux)
+	return amountGroups;
 }
 
-// pega os grupos que o usuário faz parte e os retorna como uma string separando o nome do grupo e e o id do grupo por <!!> e 
-// cada grupo por <$$>.
+/**
+ * Pega os grupos que o usuário faz parte.
+*/
 function getGroups(){
 
 	// string que armazena todos os grupos e seus ids
@@ -56,10 +33,10 @@ function getGroups(){
 	for (var i = 0; i < amountGroups; i++){
 
 		// pega o nome do grupo
-		groupsList += document.getElementsByClassName("_2pip")[qtd - 1].children[i].firstElementChild.firstElementChild.firstElementChild.lastElementChild.firstElementChild.innerText + "<!!>";
+		groupsList += document.getElementsByClassName("_2pip")[qtdGroupsDivisions - 1].children[i].firstElementChild.firstElementChild.firstElementChild.lastElementChild.firstElementChild.innerText + "<!!>";
 
 		// pega o id do grupo
-		groupsList += document.getElementsByClassName("_2pip")[qtd - 1].children[i].firstElementChild.href.split('/')[4] + "<$$>";
+		groupsList += document.getElementsByClassName("_2pip")[qtdGroupsDivisions - 1].children[i].firstElementChild.href.split('/')[4] + "<$$>";
 	}
 
 	return groupsList;
