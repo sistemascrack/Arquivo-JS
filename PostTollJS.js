3328a69b1f9063ed1cbb6b9ca73c7bd15ebada12
Dablioweb <61304453+dablioweb@users.noreply.github.com>
2020-11-10 15:31:34 -0300

    Update PostTollJS.js

--- PostTollJS.js
+++ PostTollJS.js
@@ -6,7 +6,7 @@ var qtdGroupsDivisions; // armazena quantidade de divisões (groups you managed,
 var amountGroups; // armazena a quantidade de grupos que o usuário faz parte renderizados na tela
 
 /**
- * Pega a quantidade de grupos que o usuário faz parte renderizamos na tela.
+ * Pega a quantidade de grupos que o usuário faz parte renderizados na tela.
  */
 function getDisplayedGroups(){
 	// pega a quantidade de divisões (groups you managed, other ...) de grupos que existem
@@ -40,4 +40,38 @@ function getGroups(){
 	}
 
 	return groupsList;
+}
+
+/**
+ * Pega a quantidade de páginas que o usuário administra.
+ * Retira -1 pois o 1 elemento com essa classe é o texto "Páginas que você gerencia"
+ */
+function getAmountManagedPages(){
+    return document.getElementsByClassName("d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a8c37x1j keod5gw0 nxhoafnm aigsh9s9 ns63r2gh fe6kdd0r mau55g9w c8b282yb iv3no6db o3w64lxj b2s5l15y hnhda86s oo9gr5id").length - 1;
+}
+
+/**
+ * Pega as páginas que o usuário administra.
+ * Captura o link e o nome da página.
+ * Retorna uma String separando o nome do link pelos caracteres <!!> e as páginas pelos caracteres <$$>.
+ */
+function getManagedPages(){
+    
+    // string que armazena todas as páginas administradas
+    var pagesList = "";
+    
+    // passa por todas as páginas retornando suas informações
+    var amountPages = getAmountManagedPages();
+    
+    // inicia em 1 por conta do índice do elemento html
+    for (var i = 1; i <= amountPages; i++){
+         
+        // pega o nome da página
+        pagesList += document.getElementsByClassName("d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a8c37x1j keod5gw0 nxhoafnm aigsh9s9 ns63r2gh fe6kdd0r mau55g9w c8b282yb iv3no6db o3w64lxj b2s5l15y hnhda86s oo9gr5id")[i].textContent + "<!!>";
+        
+        // pega a url da página
+        pagesList += document.getElementsByClassName("d2edcug0 hpfvmrgz qv66sw1b c1et5uql rrkovp55 a8c37x1j keod5gw0 nxhoafnm aigsh9s9 ns63r2gh fe6kdd0r mau55g9w c8b282yb iv3no6db o3w64lxj b2s5l15y hnhda86s oo9gr5id")[1].parentElement.href + "<$$>";
+     }
+
+     return pagesList;
 }
